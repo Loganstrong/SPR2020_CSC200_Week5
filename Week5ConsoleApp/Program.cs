3fafe4a5a20f5fc65d78f7b47249293956606e1c
Logan.Armstrong <Logan.Armstrong@SMU11883>
2020-02-27 10:21:02 -0800

    Call stack

--- Week5ConsoleApp/Program.cs
+++ Week5ConsoleApp/Program.cs
@@ -10,6 +10,21 @@ namespace Week5ConsoleApp
     {
         static void Main(string[] args)
         {
+            SayHello();
+            SayGoodbye();
+        }
+
+
+        public static void SayHello()
+        {
+            Console.WriteLine("Hello");
+        }
+
+
+        public static void SayGoodbye()
+        {
+            Console.WriteLine("Goodbye");
+
         }
     }
 }
